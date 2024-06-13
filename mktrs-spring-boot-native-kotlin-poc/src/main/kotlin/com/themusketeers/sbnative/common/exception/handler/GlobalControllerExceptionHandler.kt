f51ad160fb02b887b747093bf72f9c9653f71144
Carlos Adolfo Ortiz Quiros <carlosaortiz@yahoo.com>
2024-06-13 10:49:12 -0500

    Include JaCOCO config Supress Kotlin warnings when compiling

--- mktrs-spring-boot-native-kotlin-poc/src/main/kotlin/com/themusketeers/sbnative/common/exception/handler/GlobalControllerExceptionHandler.kt
+++ mktrs-spring-boot-native-kotlin-poc/src/main/kotlin/com/themusketeers/sbnative/common/exception/handler/GlobalControllerExceptionHandler.kt
@@ -102,7 +102,7 @@ class GlobalControllerExceptionHandler : ResponseEntityExceptionHandler() {
         request: WebRequest
     ): ResponseEntity<Any> {
         val instanceURL = (request as ServletWebRequest).request.requestURI // This cast is for Servlet use case.
-        val problemDetail = ProblemDetail.forStatusAndDetail(status!!, TITLE_VALIDATION_ERROR_ON_SUPPLIED_PAYLOAD)
+        val problemDetail = ProblemDetail.forStatusAndDetail(status, TITLE_VALIDATION_ERROR_ON_SUPPLIED_PAYLOAD)
 
         problemDetail.title = TITLE_BAD_REQUEST_ON_PAYLOAD
         problemDetail.type = URI.create(instanceURL)
