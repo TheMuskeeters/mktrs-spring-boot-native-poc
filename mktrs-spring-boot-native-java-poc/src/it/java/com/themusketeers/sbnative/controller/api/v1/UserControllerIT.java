3964f38295798282bfdd261102d1433870cb1af2
Carlos Adolfo Ortiz Quiros <carlosaortiz@yahoo.com>
2024-11-29 10:34:10 -0500

    Introduce
    - Configuration for running Integration tests using a separate folder, located in the 'it' folder.
    - Upgrade to SB 3.4.0

---
+++ mktrs-spring-boot-native-java-poc/src/it/java/com/themusketeers/sbnative/controller/api/v1/UserControllerIT.java
@@ -0,0 +1,26 @@
+/*----------------------------------------------------------------------------*/
+/* Source File:   USERCONTROLLERIT.JAVA                                       */
+/* Copyright (c), 2024 The Musketeers                                         */
+/*----------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------
+ History
+ Nov.30/2024  COQ  File created.
+ -----------------------------------------------------------------------------*/
+package com.themusketeers.sbnative.controller.api.v1;
+
+/**
+ * Integration test for checking {@link UserController} api.
+ * This does not make the call to the real web server.
+ * <p><b>Path:</b>{@code api/v1/users}</p>
+ * <p>Some reference links follow:
+ * <ul>
+ *     <li><a href="https://rieckpil.de/test-your-spring-mvc-controller-with-webtestclient-against-mockmvc/">Test Your Spring MVC Controller with the WebTestClient and MockMvc</a></li>
+ *     <li><a href="https://www.callicoder.com/spring-5-reactive-webclient-webtestclient-examples/">Spring 5 WebClient and WebTestClient Tutorial with Examples</a></li>
+ * </ul>
+ * </p>
+ *
+ * @author COQ- Carlos Adolfo Ortiz Q.
+ */
+class UserControllerIT {
+
+}
---
+++ mktrs-spring-boot-native-kotlin-poc/src/it/kotlin/com/themusketeers/sbnative/controller/api/v1/UserControllerIT.kt
@@ -0,0 +1,13 @@
+/*----------------------------------------------------------------------------*/
+/* Source File:   USERCONTROLLERIT.JAVA                                       */
+/* Copyright (c), 2024 The Musketeers                                         */
+/*----------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------
+ History
+ Nov.30/2024  COQ  File created.
+ -----------------------------------------------------------------------------*/
+package com.themusketeers.sbnative.controller.api.v1
+
+import org.assertj.core.api.Assertions.*
+
+class UserControllerIT
