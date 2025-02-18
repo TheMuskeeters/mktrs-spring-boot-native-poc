03abee2837ec306aacdd8217baa3545530d0961a
Carlos Adolfo Ortiz Quiros <carlosaortiz@yahoo.com>
2025-02-17 20:14:37 -0500

    Include missing dependencies

--- mktrs-spring-boot-native-kotlin-poc/src/main/kotlin/com/themusketeers/sbnative/common/consts/GlobalConstants.kt
+++ mktrs-spring-boot-native-kotlin-poc/src/main/kotlin/com/themusketeers/sbnative/common/consts/GlobalConstants.kt
@@ -24,6 +24,7 @@ object GlobalConstants {
     const val WILD_CARD_ASTERISK = "*"
     const val EMPTY_STRING = ""
     const val NULL_STRING = "null"
+    const val MOVIE_RECORD_CACHE_KEY: String = "MOVIE"
 
     const val INT_ZERO = 0
     const val INT_ONE = 1
